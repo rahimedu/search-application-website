c93e9d0a96edc33bf5a5d1c1ef092ebca6acbc33
rahimedu <edwin.duche82@gmail.com>
2022-06-23 20:16:36 -0400

    changes made in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//const searchBtn = document.getElementById('search-btn');
+const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
