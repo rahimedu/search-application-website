bd44e9384737d9f597a282715328d69bda409c4f
rahimedu <edwin.duche82@gmail.com>
2022-06-23 20:30:22 -0400

    init function getMealList

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ const recipeCloseBtn = document.getElementById('recipe-close-btn');
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
 recipeCloseBtn.addEventListener('click', () => {
-   // mealDetailsContent.parentElement.classList.remove('showRecipe');
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
 });
 
 
 // get meal list that matches with the ingredients
-function getMealList(){
+//function getMealList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
     .then(response => response.json())
