766be20338de20d4f97a0198c2274bcd2c97c3f3
rahimedu <edwin.duche82@gmail.com>
2022-06-23 20:17:33 -0400

    init meal list

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const searchBtn = document.getElementById('search-btn');
-const mealList = document.getElementById('meal');
+//const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
